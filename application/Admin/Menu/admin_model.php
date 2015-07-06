18ce5cdff2340e2c7a72d15e3b443a27b05a503c
slpi1 <1498518288@qq.com>
2015-07-06 22:29:13 +0800

    merge date(INT) and datetime(INT) to datetime(DATETIME)
    Signed-off-by: slpi1 <1498518288@qq.com>

---
+++ application/Admin/Menu/admin_model.php
@@ -0,0 +1,28 @@
+<?php
+return array (
+  'app' => 'Admin',
+  'model' => 'Model',
+  'action' => 'default',
+  'data' => '',
+  'type' => '1',
+  'status' => '1',
+  'name' => '模型管理',
+  'icon' => '',
+  'remark' => '',
+  'listorder' => '0',
+  'children' => 
+  array (
+    array (
+      'app' => 'Admin',
+      'model' => 'model',
+      'action' => 'index',
+      'data' => '',
+      'type' => '1',
+      'status' => '1',
+      'name' => '模型列表',
+      'icon' => '',
+      'remark' => '',
+      'listorder' => '0',
+    ),
+  ),
+);
