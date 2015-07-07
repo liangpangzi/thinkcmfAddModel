a3fa19a4eb4ea2b57a9fb66674ce0515f725b3de
slpi1 <1498518288@qq.com>
2015-07-07 09:49:19 +0800

    make install SQL data file
    Signed-off-by: slpi1 <1498518288@qq.com>

--- install/spcmf.sql
+++ install/spcmf.sql
@@ -56,6 +56,39 @@ CREATE TABLE `sp_auth_access` (
   KEY `rule_name` (`rule_name`) USING BTREE
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sp_attribute`
+--
+
+CREATE TABLE `sp_attribute` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL DEFAULT '' COMMENT '字段名',
+  `title` varchar(100) NOT NULL DEFAULT '' COMMENT '字段注释',
+  `field` varchar(100) NOT NULL DEFAULT '' COMMENT '字段定义',
+  `type` varchar(20) NOT NULL DEFAULT '' COMMENT '数据类型',
+  `value` varchar(100) NOT NULL DEFAULT '' COMMENT '字段默认值',
+  `remark` varchar(100) NOT NULL DEFAULT '' COMMENT '备注',
+  `is_show` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '是否显示',
+  `extra` varchar(255) NOT NULL DEFAULT '' COMMENT '参数',
+  `model_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '模型id',
+  `is_must` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否必填',
+  `status` tinyint(2) NOT NULL DEFAULT '0' COMMENT '状态',
+  `update_time` datetime DEFAULT '2000-01-01 00:00:00' COMMENT '更新时间',
+  `create_time` datetime DEFAULT '2000-01-01 00:00:00' COMMENT '创建时间',
+  `validate_rule` varchar(255) NOT NULL COMMENT '验证规则',
+  `validate_time` tinyint(1) unsigned NOT NULL COMMENT '验证时间',
+  `error_info` varchar(100) NOT NULL COMMENT '出错提示',
+  `validate_type` varchar(25) NOT NULL COMMENT '验证方式',
+  `auto_rule` varchar(100) NOT NULL COMMENT '自动完成规则',
+  `auto_time` tinyint(1) unsigned NOT NULL COMMENT '自动完成时间',
+  `auto_type` varchar(25) NOT NULL COMMENT '自动完成方式',
+  PRIMARY KEY (`id`),
+  KEY `model_id` (`model_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='模型属性表' AUTO_INCREMENT=1 ;
+
+
 -- ----------------------------
 -- Table structure for sp_auth_rule
 -- ----------------------------
@@ -188,7 +221,35 @@ CREATE TABLE `sp_menu` (
   KEY `model` (`model`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=156 ;
 
+-- --------------------------------------------------------
 
+--
+-- 表的结构 `sp_model`
+--
+
+CREATE TABLE `sp_model` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '模型ID',
+  `name` char(30) NOT NULL DEFAULT '' COMMENT '模型标识',
+  `title` char(30) NOT NULL DEFAULT '' COMMENT '模型名称',
+  `extend` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '继承的模型',
+  `relation` varchar(30) NOT NULL DEFAULT '' COMMENT '继承与被继承模型的关联字段',
+  `need_pk` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '新建表时是否需要主键字段',
+  `field_sort` text NOT NULL COMMENT '表单字段排序',
+  `field_group` varchar(255) NOT NULL DEFAULT '1:基础' COMMENT '字段分组',
+  `attribute_list` text NOT NULL COMMENT '属性列表（表的字段）',
+  `template_list` varchar(100) NOT NULL DEFAULT '' COMMENT '列表模板',
+  `template_add` varchar(100) NOT NULL DEFAULT '' COMMENT '新增模板',
+  `template_edit` varchar(100) NOT NULL DEFAULT '' COMMENT '编辑模板',
+  `list_grid` text NOT NULL COMMENT '列表定义',
+  `list_row` smallint(2) unsigned NOT NULL DEFAULT '10' COMMENT '列表数据长度',
+  `search_key` varchar(50) NOT NULL DEFAULT '' COMMENT '默认搜索字段',
+  `search_list` varchar(255) NOT NULL DEFAULT '' COMMENT '高级搜索的字段',
+  `create_time` datetime NOT NULL DEFAULT '2000-01-01 00:00:00' COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT '2000-01-01 00:00:00' COMMENT '更新时间',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '状态',
+  `engine_type` varchar(25) NOT NULL DEFAULT 'MyISAM' COMMENT '数据库引擎',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='模型表' AUTO_INCREMENT=1 ;
 
 -- 
 -- 表的结构 `sp_nav`
@@ -855,6 +916,8 @@ INSERT INTO `sp_menu` VALUES(158, 54, 'Admin', 'Slide', 'ban', '', 1, 0, '禁用
 INSERT INTO `sp_menu` VALUES(159, 54, 'Admin', 'Slide', 'cancelban', '', 1, 0, '启用幻灯片', '', '', 0);
 INSERT INTO `sp_menu` VALUES(160, 149, 'Admin', 'User', 'ban', '', 1, 0, '禁用管理员', '', '', 0);
 INSERT INTO `sp_menu` VALUES(161, 149, 'Admin', 'User', 'cancelban', '', 1, 0, '启用管理员', '', '', 0);
+INSERT INTO `sp_menu` VALUES(162, 0, 'Admin', 'Model', 'default', '', 1, 1, '模型管理', '', '', 0);
+INSERT INTO `sp_menu` VALUES(163, 162, 'Admin', 'Model', 'index', '', 1, 1, '模型列表', '', '', 0);
 -- --------------------------------------------------------
 
 -- 
